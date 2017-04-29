151490b36c811a42aa2430e53ee14ab5b6cb9247
John Stegman <greenleafservices.gls@gmail.com>
2017-04-29 22:39:09 +0000

    Updated Skills

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
