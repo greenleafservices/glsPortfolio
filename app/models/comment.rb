b23749312bcc57f275d477513b9c8b42acbfd28a
John Stegman <greenleafservices.gls@gmail.com>
2017-05-17 14:54:29 +0000

    Changed blog css colors, added comment sort by recent

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,10 @@ class Comment < ApplicationRecord
   belongs_to :blog
 
   validates :content, presence: true, length: { minimum: 5, maximimum: 1000 }
-
+  
+  def self.recent
+    order("created_at DESC")
+  end
+  
   after_create_commit { CommentBroadcastJob.perform_later(self) }
 end
