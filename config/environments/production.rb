38f1169d8a54182d6af8c817e9dd57f06242a817
John Stegman <greenleafservices.gls@gmail.com>
2017-05-17 14:14:16 +0000

    Integrated production call for Action Cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_cable.allowed_request_origins = ['https://gls-sqlportfolio.herokuapp.com', 'http://gls-sqlportfolio.herokuapp.com']
+config.action_cable.url = "wss://gls-sqlportfolio.herokuapp.com/cable"
+
 end
